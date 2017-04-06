ea1a47a53ee7b5fc62af5c7295b213d3e275f501
leo <leopold.lieb@usherbrooke.ca>
2017-04-06 13:44:13 -0400

    plateau

--- app/controller.js
+++ app/controller.js
@@ -3,7 +3,7 @@
  *
  * You can use this controller for your whole app if it is small
  * or you can have separate controllers for each logical section
- * 
+ *
  */
 ;(function() {
 
@@ -18,8 +18,28 @@
 
     // 'controller as' syntax
     var self = this;
+    var tabMeal = ["Entrée", "Plat", "Dessert"];
+    self.current = tabMeal[2];
+    self.next = tabMeal[0];
+    self.previous = tabMeal[1];
 
-
+    self.switch = function (){
+      if (tabMeal.indexOf(self.current) == 2){
+        self.current = tabMeal[0];
+      } else {
+        self.current = tabMeal[tabMeal.indexOf(self.current)+1];
+      }
+      if (tabMeal.indexOf(self.next) == 2){
+        self.next = tabMeal[0];
+      } else {
+        self.next = tabMeal[tabMeal.indexOf(self.next)+1];
+      }
+      if (tabMeal.indexOf(self.previous) == 2){
+        self.previous = tabMeal[0];
+      } else {
+        self.previous = tabMeal[tabMeal.indexOf(self.previous)+1];
+      }
+    }
     ////////////  function definitions
 
 
